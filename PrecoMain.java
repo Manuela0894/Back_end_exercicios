7c956f0cb6701da4a1a32211ed4aa72b90933ca7
Manuela Andrade <manuela.andrade894@gmail.com>
2025-08-12 09:06:20 -0300

    Update PrecoMain.java

--- PrecoMain.java
+++ PrecoMain.java
@@ -4,7 +4,8 @@ public class PrecoMain {
 public static void main(String[] args) {
 	
 	Preco compra = new Preco();
-	
+	System.out.println(compra.getPreco());
+	System.out.println(compra.getNome());
 	compra.setDesconto();
 	
 }
