0ecf44e7d48f6bbffc14f6ea189ba4a915000ca0
Manuela Andrade <manuela.andrade894@gmail.com>
2025-08-12 09:10:26 -0300

    Update PersonagensRpg.java

--- PersonagensRpg.java
+++ PersonagensRpg.java
@@ -1,11 +1,11 @@
 package aula03;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class PersonagensRpg {
 
-	private String[] personagens = {"Armin", "Enjin", "Yrva"};
+private String nome;
+	private String[] classe = {"Guerreiro", "Elfo", "Bruxa"};
 	private int poder ;
 	private int vida ;
 	private int personagemEscolhido;
@@ -13,51 +13,66 @@ public class PersonagensRpg {
 	
 	Scanner sc = new Scanner(System.in);
 
-	
-	public void escolhaPersonagem() {
-		int opcoes = 0;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome() {
+		System.out.println("Escolha um nome para seu personagem:");
+		this.nome = sc.nextLine();
+		}
 		
-		System.out.println("Escolha um dos personagens para configurar"); 
-		System.out.println("[ 1 ]- Armin  [ 2 ]- Enjin  [ 3 ]- Yrva");
-		while(true) {
-			if(sc.hasNextInt())
+	public void escolhaClasse() {
+		int opcoes = 0;
+		System.out.println("Escolha a classe do seu personagem"); 
+		System.out.println("[ 1 ]- Guerreiro  [ 2 ]- Elfo  [ 3 ]- Bruxa");
+		
+	do {
 		opcoes = sc.nextInt();
 			 if(opcoes < 1 || opcoes > 3) {
 				 System.out.println("Por favor digite um número entre 1 - 3");
 			 }else {
 			personagemEscolhido= opcoes - 1;
-			System.out.println("Personagem escolhido>> " + (personagens[personagemEscolhido]));
+			System.out.println("Classe escolhida>> " + (classe[personagemEscolhido]));
 			 }
-		  }
+		  }while(opcoes < 1 || opcoes > 3);
 		}
-	
 	public int getPoder() {
 		return poder;
 	}
 
 	public void setPoder() {
 		int poderEscolhido;
-		System.out.println("Quantidade de poder de" + (personagens[personagemEscolhido]));
+		do {
+		System.out.println("Quantidade de poder para o(a) " + (classe[personagemEscolhido]) + " " + nome + ": ");
 		poderEscolhido = sc.nextInt();
 		if(poderEscolhido < 1 || poderEscolhido > 100){
-			System.out.println("O poder precisa estar entre 1 e 0");
+			System.out.println("O poder precisa estar entre 1 e 100");
 		}else {
 		this.poder = poderEscolhido;
-		}
-		
-		
+			}
+		}while( poderEscolhido < 1 || poderEscolhido > 100 );
 	}
-//
-//	public int getVida() {
-//		return vida;
-//	}
 
-//	public void setVida() {
-//		this.vida = vida;
-//		System.out.println("Quantidade de vida" + (personagens[personagemEscolhido]));
-//		
-//	}
+	public int getVida() {
+		return vida;
+	}
 
+	public void setVida() {
+		int qntVida = 0;
+		do {
+		System.out.println("Quantidade de vida:");
+		
+		if(qntVida < 1 || qntVida > 100){
+			System.out.println("O poder precisa estar entre 1 e 100");
+		qntVida = sc.nextInt();}
+		}
+		while(qntVida < 1 || qntVida > 100);
+		this.vida = qntVida;
+	}
+
+	public void personagemFinal() {
+		System.out.printf(">> Personagem Criado!\nNome: %s%nClasse: " + classe[personagemEscolhido] +  " \nPoder:%d%nVida:%d%n",nome,poder,vida);
+	}
 	
 	
 }
